b1ee673f36717da14bce0892e96b9c55832805ef
Nikhil George <nikhilgeo@gmail.com>
2015-02-04 20:46:53 +0530

    Status code check&avi and mkv support

--- launchpad.py
+++ launchpad.py
@@ -10,7 +10,7 @@ input("If you are Online I can get you the ratings of movies in this folder, may
 
 for root, dirs, files in os.walk("."):
     for names in files:
-        if names.find('.mp4') != -1:
+        if ((names.find('.mp4') != -1) or (names.find('.avi') != -1) or (names.find('.mvk') !=-1)) :
             for year in yearlist:
                 if names.find(year) != -1:
                     trimmed_title = names[:names.find(year)+4]
@@ -24,7 +24,7 @@ for root, dirs, files in os.walk("."):
                         trimmed_title = trimmed_title.replace('[', '')
                     if trimmed_title.find(']') != -1:
                         trimmed_title = trimmed_title.replace(']', '')
-                    # print(trimmed_title)
+                    print(trimmed_title)
                     trimmed_title = trimmed_title.strip()
 
             movie_list.append(trimmed_title)
@@ -37,15 +37,16 @@ for movies in movie_list:
             title = movies[:movies.find(year)]
             # title = title.replace(' ', '+')
             payload = {'t': title, 'y': year, 'r': 'jason', 'tomatoes': 'true'}
-            response = requests.get('http://www.omdbapi.com/', params=payload).json()
-            # print(response.json().)
-            print("-----------------------------------------------")
-            print("Title:" + title)
-            print("IMDB Rating:"+str(response.get("imdbRating")))
-            print("IMDB Votes:"+str(response.get("imdbVotes")))
-            print("Tomato Meter:"+str(response.get("tomatoMeter")))
-            print("Tomato User Meter:"+str(response.get("tomatoUserMeter")))
-            #print(response.url)
-            print("-----------------------------------------------")
-
+            response = requests.get('http://www.omdbapi.com/', params=payload)
+            print(response.status_code)
+            if (response.status_code == 200):
+                jsonres = response.json()
+                print("-----------------------------------------------")
+                print("Title:" + title)
+                print("IMDB Rating:"+str(jsonres.get("imdbRating")))
+                print("IMDB Votes:"+str(jsonres.get("imdbVotes")))
+                print("Tomato Meter:"+str(jsonres.get("tomatoMeter")))
+                print("Tomato User Meter:"+str(jsonres.get("tomatoUserMeter")))
+                #print(jsonres.url)
+                print("-----------------------------------------------") 
 input("Thank you, Happy Watching Movie...")
