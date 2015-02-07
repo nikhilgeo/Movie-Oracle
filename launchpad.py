0cc509bf865e71e971649f55d2f2ffda4ed15213
Nikhil George <nikhilgeo@gmail.com>
2015-02-07 06:09:40 +0530

    CSV file output added

--- launchpad.py
+++ launchpad.py
@@ -1,13 +1,13 @@
 __author__ = 'Nikhil'
 import os
 import requests
+import csv
 
 movie_list = []
 yearlisttmp = list(range(1990, 2050))
 yearlist = [str(x) for x in yearlisttmp]
 
 input("If you are Online I can get you the ratings of movies in this folder, may I ?")
-
 for root, dirs, files in os.walk("."):
     for names in files:
         if ((names.find('.mp4') != -1) or (names.find('.avi') != -1) or (names.find('.mkv') !=-1)) :
@@ -43,12 +43,22 @@ for movies in movie_list:
             print(response.status_code)
             if (response.status_code == 200):
                 jsonres = response.json()
+                imdbRating = str(jsonres.get("imdbRating"))
+                imdbVotes = str(jsonres.get("imdbVotes"))
+                tomatoMeter = str(jsonres.get("tomatoMeter"))
+                tomatoUserMeter = str(jsonres.get("tomatoUserMeter"))
                 print("-----------------------------------------------")
                 print("Title:" + title)
-                print("IMDB Rating:"+str(jsonres.get("imdbRating")))
-                print("IMDB Votes:"+str(jsonres.get("imdbVotes")))
-                print("Tomato Meter:"+str(jsonres.get("tomatoMeter")))
-                print("Tomato User Meter:"+str(jsonres.get("tomatoUserMeter")))
+                print("IMDB Rating:"+ imdbRating)
+                print("IMDB Votes:"+ imdbVotes)
+                print("Tomato Meter:"+ tomatoMeter)
+                print("Tomato User Meter:"+ tomatoUserMeter)
                 #print(jsonres.url)
                 print("-----------------------------------------------") 
+                with open('eggs.csv', 'a') as csvfile:
+                    try:
+                        spamwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+                        spamwriter.writerow([title , imdbRating, imdbVotes, tomatoMeter, tomatoUserMeter])
+                    except csv.Error as e:
+                        sys.exit('Error %s' % (e))
 input("Thank you, Happy Watching Movie...")
